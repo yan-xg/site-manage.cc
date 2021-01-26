8087bb0407bda9eaec4c73059f5b984f1532ce6b
yanwenguang <yanwenguang@kd.cn>
2021-01-26 18:04:47 +0800

    新增主题上传七牛云

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => 'localhost',
     // 数据库名
-    'database'        => 'site_manage',
+    'database'        => 'web_manage',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => 'KDtPCaKPJZxMXP5Z',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
