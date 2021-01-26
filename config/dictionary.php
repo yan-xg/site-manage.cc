d27c1d41bc0f7b999c6bf62aaeda5d5cbdf1b005
Your Name <you@example.com>
2021-01-26 18:38:07 +0800

    提交

--- config/dictionary.php
+++ config/dictionary.php
@@ -23,7 +23,7 @@ return [
                         'mysql_detail'  => config('database.charset'),
                         'php_switch'    => 1, // PHP是否开启，默认开启。
                         'php_type'      => 'php56',
-                        'code_url'      => 'http://file.vsokgo.com/control.zip',
+                        'code_url'      => 'http://file.vsokgo.com/control.zip?time='.time(),
 //                        'domain_exist_check' => 1,// 如果有该参数，会将已经存在的网站给覆盖。
                 ],
                 'batch_upload_path' => '../uploads/site/',
@@ -36,6 +36,6 @@ return [
                                 's' => '滚动[s]', 'b' => '加粗[b]', 'p' => '图片[p]', 'j' => '跳转[j]',
                         ]
                 ],
-                'header'   => false, // 开启接口验证
+                'header'   => true, // 开启接口验证
         ]
 ];
