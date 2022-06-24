285f560877d968ce937facab4b52aa501062e8c6
kk <1694681761@qq.com>
2022-06-24 17:22:38 +0800

    site-manage

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 网站管理
+# 快道网站管理
 
 PHP版本：7.2
 mysql: 5.7
@@ -9,3 +9,9 @@ php composer install
 
 # snake
 thinkphp5.1 + layui 实现的带rbac的基础管理后台，方便快速开发法使用
+
+
+#if (!-e $request_filename) {            # 开启nginx的重写模块
+#rewrite  ^(.*)$  /index.php?s=/$1  last;
+#break;
+#}
--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '10.0.2.2',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'snake',
+    'database'        => 'site_manage',
     // 用户名
-    'username'        => 'homestead',
+    'username'        => 'root',
     // 密码
-    'password'        => 'secret',
+    'password'        => '123456',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
